b13ccd6b268cd3fbb897288ae0678ad4e94a3882
ryuilhwan <ryuilhwan@MacBook-Pro.local>
2025-06-08 15:17:39 +0900

    updates

--- packages/animals/animals.ts
+++ packages/animals/animals.ts
@@ -1,3 +1,5 @@
+import { getRandomItem } from "@tuskdesign/util";
+
 export type Animal = {
   name: string;
   sound: string;
@@ -25,10 +27,6 @@ export const ANIMALS = [
     sound: "oink",
   },
 ];
-
-export function getRandomItem<T>(arr: T[]): T {
-  return arr[Math.floor(Math.random() * arr.length)];
-}
 
 export function getRandomAnimal() {
   return getRandomItem(ANIMALS);
