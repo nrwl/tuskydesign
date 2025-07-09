f7acdb9f0adbe4926634fcb34e09e8f8281949dc
이동혁 <philip.lee@macbookpro-1.taileada8.ts.net>
2025-07-09 22:01:40 +0900

    updates

--- packages/animals/animals.ts
+++ packages/animals/animals.ts
@@ -1,35 +1,33 @@
+import { getRandomItem } from "@tuskdesign/util";
+
 export type Animal = {
-  name: string;
-  sound: string;
+	name: string;
+	sound: string;
 };
 
 export const ANIMALS = [
-  {
-    name: "dog",
-    sound: "bark",
-  },
-  {
-    name: "cat",
-    sound: "meow",
-  },
-  {
-    name: "cow",
-    sound: "moo",
-  },
-  {
-    name: "rooster",
-    sound: "cock-a-doodle-doo",
-  },
-  {
-    name: "pig",
-    sound: "oink",
-  },
+	{
+		name: "dog",
+		sound: "bark",
+	},
+	{
+		name: "cat",
+		sound: "meow",
+	},
+	{
+		name: "cow",
+		sound: "moo",
+	},
+	{
+		name: "rooster",
+		sound: "cock-a-doodle-doo",
+	},
+	{
+		name: "pig",
+		sound: "oink",
+	},
 ];
 
-export function getRandomItem<T>(arr: T[]): T {
-  return arr[Math.floor(Math.random() * arr.length)];
-}
-
 export function getRandomAnimal() {
-  return getRandomItem(ANIMALS);
+	return getRandomItem(ANIMALS);
 }
